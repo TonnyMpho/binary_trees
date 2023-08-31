474dc735839ac4a6c353f34d9e371e2d33ec2ab2
TonnyMpho <tonnympho1@gmail.com>
2023-08-31 16:01:57 -0700

    Binary trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -45,7 +45,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height != right_height)
-		return(0);
+		return (0);
 
 	if (tree->left && tree->right)
 		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));

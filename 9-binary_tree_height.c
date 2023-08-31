2548f3b96eca8ec73750c692d8bdec9b7e43cb69
TonnyMpho <tonnympho1@gmail.com>
2023-08-31 06:20:06 -0700

    Binary trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,20 +9,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_r, height_l;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	else
-	{
-		height_l = binary_tree_height(tree->left);
-		height_r = binary_tree_height(tree->right);
 
-		if (height_l >= height_r)
-			return (height_l + 1);
-		else
-			return (height_r + 1);
-	}
+	if (tree->left && tree->right)
+		return (2);
+
+	if (tree->left || tree->right)
+		return (1);
+
+	return (0);
 }
